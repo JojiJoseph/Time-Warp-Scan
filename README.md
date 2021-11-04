f33b24754f60ff8c7e48c49b8c48998e4b5a9bdc
joji <jojinattar@gmail.com>
2021-11-04 17:42:48 +0530

    Implement time warp scan in all four directions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Time-Warp-Scan
+
+To use
+
+```
+python3 scan.py -d direction
+```
+
+`direction` can be "tb","bt","lr" and "rl". Default is "tb".
+
+Example,
+
+```
+python3 scan.py -d lr
+```
--- scan.py
+++ scan.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d","--direction",default="tb", choices=["tb", "bt","lr","rl"])
+
+direction = parser.parse_args().direction
 
 cap = cv2.VideoCapture(2)
 
@@ -12,17 +19,31 @@ print(h,w)
 
 output = np.zeros((h,w,3), dtype=np.uint8)
 
-for i in range(0,h,2):
+end = h if direction in ["tb","bt"] else w
+for i in range(0,end,2):
     frame_start_time = time.time()
     ret, frame = cap.read()
-    output[i:i+2] = frame[i:i+2].copy()
-    frame[:i+2] = output[:i+2]
-    cv2.line(frame, (0,i), (w,i), (255,255,255), 2)
+    if direction == "tb":
+        output[i:i+2] = frame[i:i+2].copy()
+        frame[:i+2] = output[:i+2]
+        cv2.line(frame, (0,i), (w,i), (255,255,255), 2)
+    elif direction == "bt":
+        output[h-(i+2):h-i] = frame[h-(i+2):h-i].copy()
+        frame[h-(i+2):] = output[h-(i+2):]
+        cv2.line(frame, (0,h-i), (w,h-i), (255,255,255), 2)
+    elif direction == "lr":
+        output[:,i:i+2] = frame[:,i:i+2].copy()
+        frame[:,:i+2] = output[:,:i+2]
+        cv2.line(frame, (i,0), (i,h), (255,255,255), 2)
+    elif direction == "rl":
+        output[:,w-(i+2):w-i] = frame[:,w-(i+2):w-i].copy()
+        frame[:,w-(i+2):] = output[:,w-(i+2):]
+        cv2.line(frame, (w-i,0), (w-i,h), (255,255,255), 2)
     cv2.imshow("Input", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
-    print(1/(time.time()-frame_start_time))
+    print("fps:", 1/(time.time()-frame_start_time))
 
 cv2.imwrite("scanned.png", output)
 
